d04559d7529a679605e6173874ed5a1ee44b7a70
Ho-Sheng Hsiao <hosh@legendsoflearning.com>
2019-08-27 20:08:40 +0000

    [TEST] Modified basic tests

--- test/redix_pool_test.exs
+++ test/redix_pool_test.exs
@@ -5,47 +5,48 @@ defmodule RedixPoolTest do
   alias RedixPool, as: Redix
 
   setup do
-    Redix.command(["FLUSHDB"])
+    RedixPool.start(:normal, [pool_name: :test_pool])
+    Redix.command(:test_pool, ["FLUSHDB"])
     :ok
   end
 
   test "basic command method" do
-    assert Redix.command(["SET", "foo", "bar"]) == {:ok, "OK"}
-    assert Redix.command(["GET", "foo"]) == {:ok, "bar"}
+    assert Redix.command(:test_pool, ["SET", "foo", "bar"]) == {:ok, "OK"}
+    assert Redix.command(:test_pool, ["GET", "foo"]) == {:ok, "bar"}
 
-    assert Redix.command(["HSET", "foobar", "field", "value"]) == {:ok, 1}
-    assert Redix.command(["HGET", "foobar", "field"]) == {:ok, "value"}
+    assert Redix.command(:test_pool, ["HSET", "foobar", "field", "value"]) == {:ok, 1}
+    assert Redix.command(:test_pool, ["HGET", "foobar", "field"]) == {:ok, "value"}
   end
 
   test "basic command! method" do
-    assert Redix.command!(["SET", "foo", "bar"]) == "OK"
-    assert Redix.command!(["GET", "foo"]) == "bar"
+    assert Redix.command!(:test_pool, ["SET", "foo", "bar"]) == "OK"
+    assert Redix.command!(:test_pool, ["GET", "foo"]) == "bar"
 
-    assert Redix.command!(["HSET", "foobar", "field", "value"]) == 1
-    assert Redix.command!(["HGET", "foobar", "field"]) == "value"
+    assert Redix.command!(:test_pool, ["HSET", "foobar", "field", "value"]) == 1
+    assert Redix.command!(:test_pool, ["HGET", "foobar", "field"]) == "value"
   end
 
   test "basic pipeline method" do
-    assert Redix.pipeline([["SET", "foo", "bar"],
+    assert Redix.pipeline(:test_pool, [["SET", "foo", "bar"],
                            ["SET", "baz", "bat"]]) == {:ok, ["OK", "OK"]}
-    assert Redix.command(["GET", "foo"]) == {:ok, "bar"}
-    assert Redix.command(["GET", "baz"]) == {:ok, "bat"}
+    assert Redix.command(:test_pool, ["GET", "foo"]) == {:ok, "bar"}
+    assert Redix.command(:test_pool, ["GET", "baz"]) == {:ok, "bat"}
 
-    assert Redix.pipeline([["HSET", "foobar", "foo", "bar"],
+    assert Redix.pipeline(:test_pool, [["HSET", "foobar", "foo", "bar"],
                            ["HSET", "foobar", "baz", "bat"]]) == {:ok, [1, 1]}
-    assert Redix.command(["HGET", "foobar", "foo"]) == {:ok, "bar"}
-    assert Redix.command(["HGET", "foobar", "baz"]) == {:ok, "bat"}
+    assert Redix.command(:test_pool, ["HGET", "foobar", "foo"]) == {:ok, "bar"}
+    assert Redix.command(:test_pool, ["HGET", "foobar", "baz"]) == {:ok, "bat"}
   end
 
   test "basic pipeline! method" do
-    assert Redix.pipeline!([["SET", "foo", "bar"],
+    assert Redix.pipeline!(:test_pool, [["SET", "foo", "bar"],
                             ["SET", "baz", "bat"]]) == ["OK", "OK"]
-    assert Redix.command(["GET", "foo"]) == {:ok, "bar"}
-    assert Redix.command(["GET", "baz"]) == {:ok, "bat"}
+    assert Redix.command(:test_pool, ["GET", "foo"]) == {:ok, "bar"}
+    assert Redix.command(:test_pool, ["GET", "baz"]) == {:ok, "bat"}
 
-    assert Redix.pipeline!([["HSET", "foobar", "foo", "bar"],
+    assert Redix.pipeline!(:test_pool, [["HSET", "foobar", "foo", "bar"],
                             ["HSET", "foobar", "baz", "bat"]]) == [1, 1]
-    assert Redix.command(["HGET", "foobar", "foo"]) == {:ok, "bar"}
-    assert Redix.command(["HGET", "foobar", "baz"]) == {:ok, "bat"}
+    assert Redix.command(:test_pool, ["HGET", "foobar", "foo"]) == {:ok, "bar"}
+    assert Redix.command(:test_pool, ["HGET", "foobar", "baz"]) == {:ok, "bat"}
   end
 end
